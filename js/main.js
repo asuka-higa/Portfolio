799258dd2c9d20f564c1b34d7139a16b0ca7438f
asuka-higa <asuka.higa4study@gmail.com>
2023-09-03 15:34:58 +0900

    9/3 途中

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 {
-  /*****************
-    hamburger-menu
-  ******************/
+  /* -------------------------
+      hamburger-menuの開閉
+  ------------------------- */
 
   const hamburgerIcon = document.querySelector('.hamburger-icon');
   const dots1 = document.querySelector('.hamburger-icon-dots1');
@@ -16,8 +16,58 @@
     nav.classList.toggle('open');
   });
 
-  addEventListener('click', function () {
-    // あとで処理を書くところ
+  /* -------------------------
+          h1 矢印の動き
+  ------------------------- */
+
+  const arrow = document.querySelector('.main-arrow');
+
+  window.addEventListener('load', function () {
+    setTimeout(function () {
+      arrow.classList.toggle('load');
+    }, 3200);
   });
 
+  /* -------------------------
+          名前divの表示
+  ------------------------- */
+
+  const nameContainer = document.querySelector('.container_name');
+  const nameAlphabet = document.querySelector('.name_alphabet');
+  const nameKanji = document.querySelector('.name_kanji');
+
+  window.addEventListener('load', function () {
+    nameContainer.classList.toggle('load');
+    setTimeout(function () {
+      nameAlphabet.classList.toggle('load');
+      nameKanji.classList.toggle('load');
+    }, 600);
+  });
+
+  /* -------------------------
+        自己紹介divの表示
+  ------------------------- */
+
+  const selfIntroductionContainer = document.querySelector('.container_self-introduction');
+  const selfIntroductionText1 = document.querySelector('.self-introduction-text1');
+  const selfIntroductionText2 = document.querySelector('.self-introduction-text2');
+
+  window.addEventListener('load', function () {
+    setTimeout(function () {
+      selfIntroductionContainer.classList.toggle('load');
+      selfIntroductionText1.classList.toggle('load');
+      selfIntroductionText2.classList.toggle('load');
+    }, 1100);
+  });
+
+  /* -------------------------
+        vhの取得
+  ------------------------- */
+  const viewportHeight = window.innerHeight;
+  const vhInPixels = viewportHeight + "px";
+  const styleElement = document.createElement("style");
+  styleElement.innerHTML = `:root { --vh: ${viewportHeight}px; }`;
+  document.body.appendChild(styleElement);
 }
+
+
